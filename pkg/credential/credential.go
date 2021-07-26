47c0778fed9f96bbbaf0f9d811eafcfd47de6d1b
nickypangers <48372710+nickypangers@users.noreply.github.com>
2021-07-27 00:42:59 +0800

    Delete credential.go

--- pkg/credential/credential.go
+++
@@ -1,7 +0,0 @@
-package credential
-
-const (
-	ClientID     = "9fc05552fff74f828d684944657872de"
-	ClientSecret = "200331318a3b4b5aa7a0116ceedb8c78"
-	RedirectUrl  = "http://localhost:8080/verify"
-)
